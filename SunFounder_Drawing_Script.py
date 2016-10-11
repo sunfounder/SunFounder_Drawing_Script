ec7209a86d564d18ea953560f0aa52984a156fb1
Cavon <lijiarong@sunfounder.com>
2016-10-11 17:26:26 +0800

    add Line width

--- SunFounder_Drawing_Script.py
+++ SunFounder_Drawing_Script.py
@@ -19,7 +19,11 @@ display_res = pygame.display.Info()
 screen=pygame.display.set_mode((display_res.current_w, display_res.current_h), FULLSCREEN | NOFRAME, 32)
 #screen=pygame.display.set_mode((320, 240), NOFRAME, 32)
 
-WHITE   = (248,	248, 242)
+# Change The Line Width Here!
+LINE_WIDTH = 10
+
+
+WHITE   = (200,	200, 190)
 BLACK   = ( 39,  40,  34)
 RED	    = (249,  38, 114)
 ORANGE  = (253, 151,  31)
@@ -53,12 +57,18 @@ BLUE_RECT.center        = (EXIT_BUTTON_RECT.centerx, (GRID[1]/2) + (GRID[1] * 6)
 PURPLE_RECT.center      = (EXIT_BUTTON_RECT.centerx, (GRID[1]/2) + (GRID[1] * 7))
 CLEAR_RECT.center       = (EXIT_BUTTON_RECT.centerx, (GRID[1]/2) + (GRID[1] * 8))
 
+def draw_line(color, start_pos, end_pos):
+	#pygame.draw.aaline(screen, color, start_pos, end_pos, 1)
+
+	pygame.draw.line(screen, color, start_pos, end_pos, LINE_WIDTH)
+	pygame.draw.circle(screen, color, end_pos, LINE_WIDTH/2)
+
 def setup():
 	screen.set_clip((0, 0, display_res.current_w, display_res.current_h))
 	screen.fill(BLACK)
-	pygame.draw.rect(screen, WHITE, EXIT_BUTTON_RECT, 10)
-	pygame.draw.line(screen, WHITE, EXIT_BUTTON_RECT.topleft, EXIT_BUTTON_RECT.bottomright, 10)
-	pygame.draw.line(screen, WHITE, EXIT_BUTTON_RECT.topright, EXIT_BUTTON_RECT.bottomleft, 10)
+	pygame.draw.rect(screen, WHITE, EXIT_BUTTON_RECT, 5)
+	pygame.draw.line(screen, WHITE, EXIT_BUTTON_RECT.topleft, EXIT_BUTTON_RECT.bottomright, 5)
+	pygame.draw.line(screen, WHITE, EXIT_BUTTON_RECT.topright, EXIT_BUTTON_RECT.bottomleft, 5)
 	pygame.draw.ellipse(screen, RED, RED_RECT, 0)
 	pygame.draw.ellipse(screen, ORANGE, ORANGE_RECT, 0)
 	pygame.draw.ellipse(screen, YELLOW, YELLOW_RECT, 0)
@@ -80,11 +90,9 @@ def main():
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				exit()
-			if event.type == KEYDOWN:
-				if event.key == K_ESCAPE:
-					exit()
 			if event.type == MOUSEBUTTONDOWN:
 				start_point = pygame.mouse.get_pos()
+				pygame.draw.circle(screen, current_color, start_point, LINE_WIDTH/2)
 				Draw = True
 			if event.type == MOUSEBUTTONUP:
 				Draw = False
@@ -92,7 +100,7 @@ def main():
 
 		if Draw:
 			end_point = pygame.mouse.get_pos()
-			pygame.draw.aaline(screen, current_color, start_point, end_point, 1)
+			draw_line(current_color, start_point, end_point)
 			start_point = end_point
 		if pos != last_pos:
 			if EXIT_BUTTON_RECT.collidepoint(pos):
